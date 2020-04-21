46129f6aab7eff24c33f4b005122eafef62509ff
burakcan <brkc006@gmail.com>
2020-04-21 15:40:58 +0300

    FirstCommit

---
+++ src/main/java/com/netsis/NetsisApplication.java
@@ -0,0 +1,13 @@
+package com.netsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetsisApplication.class, args);
+	}
+
+}
